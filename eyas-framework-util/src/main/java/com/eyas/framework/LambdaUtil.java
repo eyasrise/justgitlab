3c240f1809a110314ebfdc47a7e9c54c071e6b7f
eyas <584800573@qq.com>
2019-12-04 17:24:41 +0800

    增加Lambda工具类

--- eyas-framework-util/src/main/java/com/eyas/framework/LambdaUtil.java
+++ eyas-framework-util/src/main/java/com/eyas/framework/LambdaUtil.java
@@ -1,12 +1,11 @@
 package com.eyas.framework;
 
 import lombok.Data;
+import org.omg.CORBA.PRIVATE_MEMBER;
 
+import java.io.Serializable;
 import java.lang.reflect.Field;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
+import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.Function;
 import java.util.function.Predicate;
@@ -21,6 +20,8 @@ public class LambdaUtil {
     private Long id;
 
     private String code;
+
+    private Integer age;
 
     /**
      * list去重算法
@@ -50,5 +51,54 @@ public class LambdaUtil {
     private static <T> Predicate<T> distinctByKey(Function<? super T, Object> keyExtractor) {
         Map<Object, Boolean> seen = new ConcurrentHashMap<>();
         return object -> seen.putIfAbsent(keyExtractor.apply(object), Boolean.TRUE) == null;
+    }
+
+    /**
+     * 排序工具——由小到大
+     *
+     * @param tList 排序集合
+     * @param keyExtractor 排序字段
+     * @param <T> 忽略
+     * @param <U> 忽略
+     * @return 排序结果
+     */
+    public static <T, U extends Comparable<? super U>> List<T> sort01(List<T> tList, Function<? super T, ? extends U> keyExtractor){
+        //list是需要去重的list，返回值是去重后的list
+        List<T> distinctTs = tList.stream().sorted(Comparator.comparing(keyExtractor)).collect(Collectors.toList());
+        return distinctTs;
+    }
+
+    /**
+     * 排序工具——由大到小
+     *
+     * @param tList 排序集合
+     * @param keyExtractor 排序字段
+     * @param <T> 忽略
+     * @param <U> 忽略
+     * @return 排序结果
+     */
+    public static <T, U extends Comparable<? super U>> List<T> sort10(List<T> tList, Function<? super T, ? extends U> keyExtractor){
+        //list是需要去重的list，返回值是去重后的list
+        List<T> distinctTs = tList.stream().sorted(Comparator.comparing(keyExtractor).reversed()).collect(Collectors.toList());
+        return distinctTs;
+    }
+
+
+    public static void main(String[] args) {
+        List<LambdaUtil> lambdaUtilList = new ArrayList<>();
+        LambdaUtil lambdaUtil = new LambdaUtil();
+        lambdaUtil.setId(1L);
+        lambdaUtil.setAge(10);
+        LambdaUtil lambdaUtil2 = new LambdaUtil();
+        lambdaUtil2.setId(2L);
+        lambdaUtil2.setAge(30);
+        LambdaUtil lambdaUtil3 = new LambdaUtil();
+        lambdaUtil3.setId(3L);
+        lambdaUtil3.setAge(20);
+        lambdaUtilList.add(lambdaUtil);
+        lambdaUtilList.add(lambdaUtil2);
+        lambdaUtilList.add(lambdaUtil3);
+        System.out.println(LambdaUtil.sort10(lambdaUtilList, LambdaUtil::getAge));
+        System.out.println(LambdaUtil.sort01(lambdaUtilList, LambdaUtil::getAge));
     }
 }
