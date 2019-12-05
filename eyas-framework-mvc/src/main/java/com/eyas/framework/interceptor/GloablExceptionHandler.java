9d1f04217d17c069f6b4a550b27a5e927194ac44
eyas <584800573@qq.com>
2019-12-05 14:40:24 +0800

    增加空指针异常注解报错异常处理

--- eyas-framework-mvc/src/main/java/com/eyas/framework/interceptor/GloablExceptionHandler.java
+++ eyas-framework-mvc/src/main/java/com/eyas/framework/interceptor/GloablExceptionHandler.java
@@ -4,9 +4,13 @@ import com.eyas.framework.data.EyasFrameworkResult;
 import com.eyas.framework.enumeration.ErrorFrameworkCodeEnum;
 import com.eyas.framework.exception.EyasFrameworkRuntimeException;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 /**
  * @author Created by yixuan on 2019/7/11.
@@ -14,6 +18,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @ControllerAdvice
 @Slf4j
 public class GloablExceptionHandler {
+
     @ResponseBody
     @ExceptionHandler(Exception.class)
     public Object handleException(Exception e) {
@@ -34,5 +39,17 @@ public class GloablExceptionHandler {
         }
         log.info(e.getCode() + "-" + msg);
         return EyasFrameworkResult.fail(e.getCode(), e.getMsg());
+    }
+
+    @ResponseBody
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Object handleIllegalParamException(MethodArgumentNotValidException e) {
+        List<ObjectError> errors = e.getBindingResult().getAllErrors();
+        String tips = "参数不合法";
+        if (!errors.isEmpty()) {
+            tips = errors.get(0).getDefaultMessage();
+        }
+        log.error(tips, e);
+        return EyasFrameworkResult.fail(ErrorFrameworkCodeEnum.NULL_PARAM_ERROR.getErrCode(), tips);
     }
 }
