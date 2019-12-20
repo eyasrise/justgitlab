22a3024dfda7401b3ed47f5ee2a9f01e9d5b7c30
eyas <584800573@qq.com>
2019-12-20 11:22:21 +0800

    修改单词拼写

--- eyas-framework-mvc/src/main/java/com/eyas/framework/config/Swagger2Config.java
+++ eyas-framework-mvc/src/main/java/com/eyas/framework/config/Swagger2Config.java
@@ -1,6 +1,6 @@
 package com.eyas.framework.config;
 
-
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
@@ -11,15 +11,20 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+
 @Configuration
 @EnableSwagger2
 public class Swagger2Config {
+
+    @Value("${spring.application.simple}")
+    private String applicationName;
+
     @Bean
     public Docket createRestApi() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiInfo())
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.eyas.framework.controller"))
+                .apis(RequestHandlerSelectors.basePackage("com.eyas."+applicationName+".controller"))
                 .paths(PathSelectors.any())
                 .build();
     }
