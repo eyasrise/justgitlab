f65f61d5d69848575a468dd67a7c50fb8e2b5e8c
eyas <584800573@qq.com>
2019-12-23 10:00:55 +0800

    增加架构基础对象字段描述

--- eyas-framework-common/src/main/java/com/eyas/framework/data/EyasFrameworkDto.java
+++ eyas-framework-common/src/main/java/com/eyas/framework/data/EyasFrameworkDto.java
@@ -1,5 +1,6 @@
 package com.eyas.framework.data;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import java.io.Serializable;
@@ -12,59 +13,71 @@ import java.util.Date;
 public class EyasFrameworkDto implements Serializable {
 
     private static final long serialVersionUID = -7779110672848186411L;
+
     /**
      * 行号
      */
+    @ApiModelProperty(value = "行号")
     private Long id;
 
     /**
      * 业务主键
      */
+    @ApiModelProperty(value = "业务主键")
     private String code;
 
     /**
      * 数据业务状态
      */
+    @ApiModelProperty(value = "数据业务状态")
     private Integer status;
 
     /**
      * 数据类型
      */
+    @ApiModelProperty(value = "数据类型")
     private Integer type;
 
     /**
      * 创建时间
      */
+    @ApiModelProperty(value = "创建时间")
     private Date createTime;
 
     /**
      * 修改时间
      */
+    @ApiModelProperty(value = "修改时间")
     private Date updateTime;
 
     /**
      * 创建人
      */
+    @ApiModelProperty(value = "创建人")
     private String creator;
 
     /**
      * 修改人
      */
+    @ApiModelProperty(value = "修改人")
     private String operator;
 
     /**
      * 备注
      */
+    @ApiModelProperty(value = "备注")
     private String remark;
 
     /**
      * 备用字段
      */
+    @ApiModelProperty(value = "备用字段")
     private String extAtt;
 
     /**
      * 项目编码code
      */
+    @ApiModelProperty(value = "项目编码code")
     private Long projectCode;
 
 }
--- eyas-framework-common/src/main/java/com/eyas/framework/data/EyasFrameworkQuery.java
+++ eyas-framework-common/src/main/java/com/eyas/framework/data/EyasFrameworkQuery.java
@@ -1,5 +1,6 @@
 package com.eyas.framework.data;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import java.io.Serializable;
@@ -17,72 +18,86 @@ public class EyasFrameworkQuery extends EyasFrameworkBaseQuery implements Serial
     /**
      * 行号
      */
+    @ApiModelProperty(value = "行号")
     private Long id;
 
     /**
      * 业务主键
      */
+    @ApiModelProperty(value = "业务主键")
     private String code;
 
     /**
      * 数据业务状态
      */
+    @ApiModelProperty(value = "数据业务状态")
     private Integer status;
 
     /**
      * 数据类型
      */
+    @ApiModelProperty(value = "数据类型")
     private Integer type;
 
     /**
      * 创建时间
      */
+    @ApiModelProperty(value = "创建时间")
     private Date createTime;
 
     /**
      * 修改时间
      */
+    @ApiModelProperty(value = "修改时间")
     private Date updateTime;
 
     /**
      * 创建人
      */
+    @ApiModelProperty(value = "创建人")
     private String creator;
 
     /**
      * 修改人
      */
+    @ApiModelProperty(value = "修改人")
     private String operator;
 
     /**
      * 备注
      */
+    @ApiModelProperty(value = "备注")
     private String remark;
+
+    /**
+     * 备用字段
+     */
+    @ApiModelProperty(value = "备用字段")
+    private String extAtt;
+
+    /**
+     * 项目编码code
+     */
+    @ApiModelProperty(value = "项目编码code")
+    private Long projectCode;
 
     // 业务字段
 
     /**
      * id多值
      */
+    @ApiModelProperty(value = "id多值")
     private List<String> ids;
 
     /**
      * status多值
      */
+    @ApiModelProperty(value = "status多值")
     private String statusStr;
 
     /**
      * status多值
      */
+    @ApiModelProperty(value = "status多值")
     private List<String> statusInt;
-
-    /**
-     * 备用字段
-     */
-    private String extAtt;
-
-    /**
-     * 项目编码code
-     */
-    private Long projectCode;
 }
