92541e2d92a959fea0dbf5b4417b3700fba922b3
wdy0707 <1150828564@qq.com>
2020-03-30 15:04:23 +0800

    增加token续期

--- eyas-framework-mvc/src/main/java/com/eyas/framework/interceptor/AuthenticationInterceptor.java
+++ eyas-framework-mvc/src/main/java/com/eyas/framework/interceptor/AuthenticationInterceptor.java
@@ -3,6 +3,7 @@ package com.eyas.framework.interceptor;
 import com.auth0.jwt.exceptions.JWTDecodeException;
 import com.eyas.framework.JwtUtils;
 import com.eyas.framework.annotation.WithOutToken;
+import com.eyas.framework.constant.SystemConstant;
 import com.eyas.framework.enumeration.ErrorFrameworkCodeEnum;
 import com.eyas.framework.exception.EyasFrameworkRuntimeException;
 import com.eyas.framework.impl.RedisServiceImpl;
@@ -16,6 +17,7 @@ import org.springframework.web.servlet.ModelAndView;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.lang.reflect.Method;
+import java.util.Date;
 
 /**
  * @author Created by yixuan on 2019/7/11.
