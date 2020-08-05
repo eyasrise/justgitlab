a17f0c15ff867cc9e29c4539a3e696fa65d540d6
feixiansheng <838854701@qq.com>
2020-08-05 09:05:40 +0800

    okhttp3

---
+++ eyas-framework-config/src/main/java/com/eyas/framework/config/RestETemplateConfig.java
@@ -0,0 +1,20 @@
+package com.eyas.framework.config;
+
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.OkHttp3ClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class RestETemplateConfig {
+//    @Bean
+//    public RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder) {
+//        return restTemplateBuilder.build();
+//    }
+
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder) {
+        return restTemplateBuilder.requestFactory(OkHttp3ClientHttpRequestFactory.class).build();
+    }
+}
