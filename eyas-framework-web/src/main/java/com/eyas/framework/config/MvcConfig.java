2db4144902fc92baa2762573d057f9e721325827
eyas <eyasrise@gmail.com>
2021-03-18 11:37:43 +0800

    架构2.0jwt修改

--- eyas-framework-web/src/main/java/com/eyas/framework/config/MvcConfig.java
+++ eyas-framework-web/src/main/java/com/eyas/framework/config/MvcConfig.java
@@ -1,7 +1,10 @@
 package com.eyas.framework.config;
 
 import com.eyas.framework.interceptor.AuthenticationInterceptor;
+import com.eyas.framework.interceptor.UserInfo;
+import com.eyas.framework.provider.UserProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -13,10 +16,10 @@ public class MvcConfig implements WebMvcConfigurer {
     private AuthenticationInterceptor authenticationInterceptor;
 
 
-//    @Bean
-//    UserProvider userProvider(){
-//        return (userId, tokenInfo) -> UserInfo.builder().userId("XS1212").userCode("1212").tenantCode(100L).systemUser(null).build();
-//    }
+    @Bean
+    UserProvider userProvider(){
+        return (userId, tokenInfo) -> UserInfo.builder().userId("XS1212").userCode("1212").tenantCode(100L).systemUser(null).build();
+    }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
