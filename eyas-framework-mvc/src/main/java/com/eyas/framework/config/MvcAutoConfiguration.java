225f10f29871a4096654da8710ffe0029cc875bb
gaojian <546342424@qq.com>
2021-03-18 10:04:06 +0800

    用户信息扩展

--- eyas-framework-mvc/src/main/java/com/eyas/framework/config/MvcAutoConfiguration.java
+++ eyas-framework-mvc/src/main/java/com/eyas/framework/config/MvcAutoConfiguration.java
@@ -6,7 +6,9 @@ import com.eyas.framework.provider.UserProvider;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class MvcAutoConfiguration {
 
     @Bean
