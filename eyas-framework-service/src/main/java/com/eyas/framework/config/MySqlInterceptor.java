ed48bb75d4fb90e78ded5bc260df40e916bfe8d0
eyas <eyasrise@gmail.com>
2021-03-25 09:28:25 +0800

    架构2.0升级

--- eyas-framework-service/src/main/java/com/eyas/framework/config/MySqlInterceptor.java
+++ eyas-framework-service/src/main/java/com/eyas/framework/config/MySqlInterceptor.java
@@ -1,5 +1,4 @@
-package com.eyas.ptw.config;
-
+package com.eyas.framework.config;
 
 import com.eyas.framework.EmptyUtil;
 import com.eyas.framework.data.EyasFrameworkDto;
@@ -17,6 +16,7 @@ import net.sf.jsqlparser.statement.Statements;
 import net.sf.jsqlparser.statement.insert.Insert;
 import net.sf.jsqlparser.statement.select.PlainSelect;
 import net.sf.jsqlparser.statement.select.Select;
+import net.sf.jsqlparser.statement.select.SelectItem;
 import org.apache.ibatis.executor.statement.StatementHandler;
 import org.apache.ibatis.mapping.BoundSql;
 import org.apache.ibatis.plugin.*;
@@ -26,7 +26,9 @@ import org.apache.ibatis.reflection.SystemMetaObject;
 
 import java.io.StringReader;
 import java.sql.Connection;
+import java.util.List;
 import java.util.Properties;
+import java.util.concurrent.atomic.AtomicReference;
 
 /**
  * @author Created by eyas on 2021/2/22.
@@ -34,113 +36,86 @@ import java.util.Properties;
 @Intercepts({@Signature(type = StatementHandler.class, method = "prepare", args = {Connection.class, Integer.class})})
 @Slf4j
 public class MySqlInterceptor implements Interceptor {
-    @Override
+
+    public MySqlInterceptor() {
+    }
+
     public Object intercept(Invocation invocation) throws Throwable {
-        StatementHandler statementHandler = (StatementHandler) invocation.getTarget();
+        StatementHandler statementHandler = (StatementHandler)invocation.getTarget();
         MetaObject metaObject = MetaObject.forObject(statementHandler, SystemMetaObject.DEFAULT_OBJECT_FACTORY, SystemMetaObject.DEFAULT_OBJECT_WRAPPER_FACTORY, new DefaultReflectorFactory());
-        //先拦截到RoutingStatementHandler，里面有个StatementHandler类型的delegate变量，其实现类是BaseStatementHandler，然后就到BaseStatementHandler的成员变量mappedStatement
         BoundSql boundSql = statementHandler.getBoundSql();
-
-        //获取到原始sql语句
         String sql = boundSql.getSql();
-
         StringBuffer whereSql = new StringBuffer();
         CCJSqlParserManager parserManager = new CCJSqlParserManager();
-
-
         Statements statements = CCJSqlParserUtil.parseStatements(sql);
-
-        if (statements.getStatements().get(0) instanceof Select){
-            Select select = (Select) parserManager.parse(new StringReader(sql));
-            PlainSelect plain = (PlainSelect) select.getSelectBody();
-            EyasFrameworkDto systemUser = (EyasFrameworkDto) TenantThreadLocal.getSystemUser();
-
-
-            if (systemUser !=null){
+        if (statements.getStatements().get(0) instanceof Select) {
+            Select select = (Select)parserManager.parse(new StringReader(sql));
+            PlainSelect plain = (PlainSelect)select.getSelectBody();
+            EyasFrameworkDto systemUser = (EyasFrameworkDto)TenantThreadLocal.getSystemUser();
+            List<SelectItem> selectItemList = plain.getSelectItems();
+            AtomicReference<Boolean> flag = new AtomicReference<>(false);
+            selectItemList.stream().forEach(selectItem -> {
+                if (selectItem.toString().equals("TENANT_CODE")){
+                    flag.set(true);
+                }
+            });
+            if (systemUser != null && flag.get()) {
                 whereSql.append("TENANT_CODE =");
                 whereSql.append(systemUser.getTenantCode());
             }
 
-            //--todo--需要获取动态租户code的逻辑--
-
-            // 获取当前查询条件
             Expression where = plain.getWhere();
+            Expression expression;
             if (where == null) {
                 if (whereSql.length() > 0) {
-                    Expression expression = CCJSqlParserUtil
-                            .parseCondExpression(whereSql.toString());
+                    expression = CCJSqlParserUtil.parseCondExpression(whereSql.toString());
                     plain.setWhere(expression);
                 }
             } else {
-                // 判断plain.getJoins()是否为空，用来判定是不是联表查询，因为联表查询比较复杂，所有直接忽略联表查询
-                // 链表查询自己写租户关联逻辑
                 if (whereSql.length() > 0 && EmptyUtil.isEmpty(plain.getJoins())) {
-                    //where条件之前存在，需要重新进行拼接
                     whereSql.append(" and ( ").append(where.toString()).append(" )");
                 } else {
-                    //新增片段不存在，使用之前的sql
                     whereSql = new StringBuffer();
                     whereSql.append(where.toString());
                 }
-                Expression expression = CCJSqlParserUtil
-                        .parseCondExpression(whereSql.toString());
+
+                expression = CCJSqlParserUtil.parseCondExpression(whereSql.toString());
                 plain.setWhere(expression);
             }
+
             metaObject.setValue("delegate.boundSql.sql", select.toString());
             log.info("新sql===>" + select.toString());
             return invocation.proceed();
-        }
-        if (statements.getStatements().get(0) instanceof Insert){
-            EyasFrameworkDto systemUser = (EyasFrameworkDto) TenantThreadLocal.getSystemUser();
-            if(EmptyUtil.isEmpty(systemUser)){
+        } else if (statements.getStatements().get(0) instanceof Insert) {
+            EyasFrameworkDto systemUser = (EyasFrameworkDto)TenantThreadLocal.getSystemUser();
+            if (EmptyUtil.isEmpty(systemUser)) {
+                return invocation.proceed();
+            } else {
+                Long tenantCode = systemUser.getTenantCode();
+                Insert insert = (Insert)parserManager.parse(new StringReader(sql));
+                insert.getColumns().add(new Column("TENANT_CODE"));
+                if (insert.getItemsList() != null) {
+                    ItemsList itemsList = insert.getItemsList();
+                    if (itemsList instanceof MultiExpressionList) {
+                        ((MultiExpressionList)itemsList).getExprList().forEach((dto) -> {
+                            dto.getExpressions().add(new LongValue(tenantCode));
+                        });
+                    } else {
+                        ((ExpressionList)insert.getItemsList()).getExpressions().add(new LongValue(tenantCode));
+                    }
+                }
+
                 return invocation.proceed();
             }
-
-            Long tenantCode = systemUser.getTenantCode();
-
-            Insert insert = (Insert) parserManager.parse(new StringReader(sql));
-
-            insert.getColumns().add(new Column("TENANT_CODE"));
-            if (insert.getItemsList()!=null){
-                ItemsList itemsList = insert.getItemsList();
-                if (itemsList instanceof MultiExpressionList){
-                    ((MultiExpressionList)itemsList).getExprList().forEach(dto-> dto.getExpressions().add(new LongValue(tenantCode)));
-                }else {
-                    ((ExpressionList) insert.getItemsList()).getExpressions().add(new LongValue(tenantCode));
-                }
-            }
-
-            // PlainSelect plain = (PlainSelect) insert.getSelect().getSelectBody();
-
-
-
-
-//            Expression expression = CCJSqlParserUtil
-//                    .parseCondExpression(whereSql.toString());
-//            plain.setWhere(expression);
-
-            return invocation.proceed();
         } else {
-            //返回原始sql
             return invocation.proceed();
-
         }
-
-
-
     }
 
-    @Override
     public Object plugin(Object o) {
         return Plugin.wrap(o, this);
     }
 
-    @Override
     public void setProperties(Properties properties) {
-
     }
-
-
-
-
 }
