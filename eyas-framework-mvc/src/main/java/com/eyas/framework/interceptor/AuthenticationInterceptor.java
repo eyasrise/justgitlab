02512846b42adad5a930de4cba0c5ff82c3f6370
feixiansheng <838854701@qq.com>
2021-03-25 16:03:24 +0800

    时间续期

--- eyas-framework-mvc/src/main/java/com/eyas/framework/interceptor/AuthenticationInterceptor.java
+++ eyas-framework-mvc/src/main/java/com/eyas/framework/interceptor/AuthenticationInterceptor.java
@@ -87,7 +87,7 @@ public class AuthenticationInterceptor implements HandlerInterceptor {
             }
             //对token的过期时间进行判断，续期
             Date expiration = claims.getExpiration();
-            if (expiration.getTime() - System.currentTimeMillis() < 1800_000) {
+            if (expiration.getTime() - System.currentTimeMillis() < 1800_000*6) {
                 String newToken = JwtUtils.createJWT(userId, tenantCode.toString(), SystemConstant.JWT_TTL);
                 httpServletResponse.setHeader("token", newToken);
             }
